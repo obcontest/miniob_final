7492cc2a4bd4d26a8f8c2cbbe469eb0b65511299
Wind-Gone <1242378947@qq.com>
2022-04-28 04:06:09 +0800

    fix: delete trx bugs

--- src/observer/storage/default/disk_buffer_pool.h
+++ src/observer/storage/default/disk_buffer_pool.h
@@ -33,7 +33,7 @@ typedef int PageNum;
 #define BP_PAGE_SIZE (1 << 18)
 #define BP_PAGE_DATA_SIZE (BP_PAGE_SIZE - sizeof(PageNum))
 #define BP_FILE_SUB_HDR_SIZE (sizeof(BPFileSubHeader))
-#define BP_BUFFER_SIZE (1 << 14)
+#define BP_BUFFER_SIZE 256
 #define MAX_OPEN_FILE 1024
 
 typedef struct {
