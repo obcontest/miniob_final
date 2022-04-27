74633e28157921eab5e802c69d15af79c46e6d7e
not_exists <not_exists@localhost.com>
2022-04-27 15:03:47 +0800

    [fix] fix script bug

--- compression.sh
+++ compression.sh
@@ -7,4 +7,4 @@ if [ $# -eq 1 ]; then
 fi
 
 echo "create table t1 (c1 int, c2 int, c3 int, v1 char, v2 char, v3 char, v4 char, v5 char, v6 char, v7 char, v8 char, v9 char);" | ./cmake-build-debug/bin/obclient
-echo "load data infile '${pwd}/data/miniob-${table_size}-v2.table' into table t1;"
+echo "load data infile '$PWD/data/miniob-${table_size}-v2.table' into table t1;" | ./cmake-build-debug/bin/obclient
